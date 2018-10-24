2dc7868eaf692e1c8a7f32b8f6b8b025f01c8950
IT-40016\user <yakovenkovl12@gmail.com>
2018-10-24 17:41:31 +0300

    Added comments to public functions

--- ua/edu/sumdu/j2se/vladislavY/tasks/ArrayTaskList.java
+++ ua/edu/sumdu/j2se/vladislavY/tasks/ArrayTaskList.java
@@ -13,19 +13,28 @@ public class ArrayTaskList {
         this.list = new Task[1];
     }
 
+    /**
+     * Adds new task to the end of the tasks list
+     * @param task task for adding to the tasks list
+     */
     public void add(Task task) {
-        if (list.length - 1 == counter) {
+        if (list.length - 1 == this.size()) {
             Task[] listHelper = list;
-            list = new Task[counter + 2];
+            list = new Task[this.size() + 2];
             System.arraycopy(listHelper, 0, list, 0, listHelper.length);
         }
         list[counter++] = task;
     }
 
+    /**
+     * Removes task from the list, if list contains several the same tasks - the first one will be removed
+     * @param task searched task
+     * @return true - if searched task was found and deleted, else false
+     */
     public boolean remove(Task task) {
-        for (int i = 0; i < list.length; i++) {
+        for (int i = 0; i < this.size(); i++) {
             if (list[i] == task) {
-                while (i < list.length - 1) {
+                while (i < this.size() - 1) {
                     list[i] = list[++i];
                 }
                 list[i] = null;
@@ -36,10 +45,19 @@ public class ArrayTaskList {
         return false;
     }
 
+    /**
+     * Retunns amount of tasks
+     * @return number of tasks in the array
+     */
     public int size() {
         return counter;
     }
 
+    /**
+     * Returns taks by index
+     * @param index task index
+     * @return task from array by index
+     */
     public Task getTask(int index) {
         return list[index];
     }
